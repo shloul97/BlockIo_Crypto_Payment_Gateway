9b0b530c0deb56edb52074981943b14c8ab420e3
Moayad Shloul <shloul97@gmail.com>
2023-12-24 04:39:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -25,14 +25,10 @@ $dog_rate = new coinAPI('dog');
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
-        <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-        <meta name="generator" content="Hugo 0.118.2">
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link href="style/style.css" rel="stylesheet">
-        <link href="style/sidebars.css" rel="stylesheet">
-        <link href="style/contentStyle.css" rel="stylesheet">
         <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sidebars/">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
